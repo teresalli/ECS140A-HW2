5c2801a91bae351cf79919f36cbc8073d7bedea4
teresalli <sqtli@ucdavis.edu>
2018-04-13 11:32:22 -0700

    find the line

--- src/analysis/rewrite.go
+++ src/analysis/rewrite.go
@@ -6,13 +6,36 @@ import (
 	"go/format"
 	"go/parser"
 	"go/token"
-	// "eval"
-	// "strconv"
+  //"strings"
+  "fmt"
+	"eval"
+	"strconv"
 )
+
+type FuncVisitor struct {
+}
+
+func (v *FuncVisitor) Visit(node ast.Node) (w ast.Visitor)  {
+  switch call := node.(type) {
+  case *ast.CallExpr:
+    if name,ok := call.Fun.(*ast.SelectorExpr); ok{
+      // function call found
+      if name.Sel.Name == "ParseAndEval" {
+        if item,ok := call.Args[0].(*ast.BasicLit); ok{
+          num,_ := eval.ParseAndEval(item.Value, eval.Env{})
+          fmt.Println(item.Value)
+          item.Value = strconv.Itoa(int(num))
+        }
+      }
+    }
+  }
+
+  return v
+}
 
 // rewriteCalls should modify the passed AST
 func rewriteCalls(node ast.Node) {
-
+  ast.Walk(new(FuncVisitor), node)
 }
 
 func SimplifyParseAndEval(src string) string {
@@ -21,8 +44,9 @@ func SimplifyParseAndEval(src string) string {
 	if err != nil {
 		panic(err)
 	}
-
+  //ast.Print(fset, f)
 	rewriteCalls(f)
+  //ast.Print(fset, f)
 
 	var buf bytes.Buffer
 	format.Node(&buf, fset, f)
